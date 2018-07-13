c95253d5a53b61c13935739dcf8d68368423c6e8
1670599538 <1670599538@qq.com>
2018-07-13 18:51:10 +0800

    景观景区转化成分类和产品

---
+++ src/main/java/data/driven/business/controller/material/MatCategoryController.java
@@ -0,0 +1,42 @@
+package data.driven.business.controller.material;
+
+import com.alibaba.fastjson.JSONObject;
+import data.driven.business.business.material.MatCategoryService;
+import data.driven.business.controller.wechatapi.ScenicSpotApiController;
+import data.driven.business.entity.material.MatCategoryEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author hejinkai
+ * @date 2018/7/13
+ */
+@Controller
+@RequestMapping(path = "/material/category")
+public class MatCategoryController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScenicSpotApiController.class);
+
+    @Autowired
+    private MatCategoryService matCategoryService;
+
+    @ResponseBody
+    @RequestMapping(path = "/getMatCategory")
+    public JSONObject getMatCategory(String catgId){
+        JSONObject result = new JSONObject();
+        MatCategoryEntity matCategory = matCategoryService.getMatCategoryNoUser(catgId);
+        if(matCategory != null){
+            result.put("success", true);
+            result.put("msg", matCategory.getCatgName());
+        }else{
+            result.put("success", false);
+            result.put("msg", "查询的分类信息为空");
+        }
+        return result;
+    }
+
+}
