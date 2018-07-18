8efce35c0114f2e963372203739b18e272c69f7a
1670599538 <1670599538@qq.com>
2018-07-18 19:04:24 +0800

    修改根据用户查询app信息

--- src/main/java/data/driven/business/business/wechat/impl/WechatAppInfoServiceImpl.java
+++ src/main/java/data/driven/business/business/wechat/impl/WechatAppInfoServiceImpl.java
@@ -30,7 +30,8 @@ public class WechatAppInfoServiceImpl implements WechatAppInfoService {
 
     @Override
     public List<WechatAppInfoEntity> findAppInfoListByUser(String userInfoId) {
-        String sql = "select app_info_id,app_name,appid,secret,create_at,creator from wechat_app_info where appid = ?";
+        String sql = "select wai.app_info_id,wai.app_name,wai.appid,wai.secret,wai.create_at,wai.creator from wechat_app_info wai" +
+                " left join sys_user_wechat_app_mapping suwam on suwam.app_info_id = wai.app_info_id where suwam.user_id = ?";
         List<WechatAppInfoEntity> list = jdbcBaseDao.queryList(WechatAppInfoEntity.class, sql, userInfoId);
         return list;
     }
